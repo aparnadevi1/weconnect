8b139a9d8ee909dfab8aad9a82e433082397ee78
Aparna <chodapuneediaparnadevi@gmail.com>
2025-08-26 16:18:30 +0530

    add validation in schema

--- src/app.js
+++ src/app.js
@@ -56,7 +56,7 @@ app.patch("/user",async(req,res)=>
     const userId=req.body.userId;
     const data=req.body;
     try{
-       const user=await User.findByIdAndUpdate({_id:userId},data,{returnDocument:"after"});
+       const user=await User.findByIdAndUpdate({_id:userId},data,{returnDocument:"after",runValidators:true});
        res.send({message:"user updated successfully",user});
 
     }
@@ -64,7 +64,7 @@ app.patch("/user",async(req,res)=>
        res.status(400).send("Error updating the data"+err.message);
     }
 
-})
+});
 app.delete("/user",async(req,res)=>{
     
    const userId=req.body.userId;
--- src/models/user.js
+++ src/models/user.js
@@ -1,24 +1,51 @@
 const mongoose=require("mongoose");
 const userSchema=new mongoose.Schema({
        firstName:{
-        type:String
+        type:String,
+        required:true,
+        minLength:4,
+        maxLength:50
        },
        lastName:{
         type:String
        },
        emailId:{
-        type:String
+        type:String,
+        lowercase:true,
+        trim:true,
+        required:true,
+        unique:true
        },
        password:{
-        type:String
+        type:String,
+        required:true,
        },
        age:{
-        type: Number
+        type: Number,
+        min:18,
+        
        },
        gender:{
-        type:String
+        type:String,
+        validate(value){
+            if(!["male","female", "others"].includes(value))
+            {
+                throw new Error("Gender data is not valid");
+            }
+        },
+       },
+       photoUrl:{
+        type:String,
+        default:"https://www.pinclipart.com/picdir/middle/541-5416602_chin-clipart.png"
+       },
+       about:{
+        type:String,
+        default:"This is a default about of the user!"
+       },
+       skills:{
+        type:[String],
        }
 
-});
+},{timestamps:true});
 const User=mongoose.model("User",userSchema);
 module.exports=User;
